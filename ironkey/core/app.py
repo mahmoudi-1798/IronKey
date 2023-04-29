a06bc9c1beee1deabc83f83cf1fcf35f1cc583a7
amir <amir123fg5@gmail.com>
2023-04-29 11:32:00 +0330

    ADD: add command option "update"
    this command take input for update it 
    and if it is exist then user give another input to update and choose option to update password

--- ironkey/core/app.py
+++ ironkey/core/app.py
@@ -8,8 +8,9 @@ class Commands:
     
     def init(self):
         check = db.check_user_exist()
-        if check:
-            return print("User already exist")
+        if check is True:
+            print("User already exist")
+            return 
         else:
             name = interface.get_info(name="username", prefix="choose a")
             if name == "ex" or name == "exit":
@@ -24,7 +25,7 @@ class Commands:
         if check:
             return print("Oops this title already exist")
         else:
-            password = interface.get_info(name="password", prefix="inter the")
+            password = interface.get_info(name="password", prefix="inter the", hide=True)
             db.add_password(title, password)
     
     def generate(self):
@@ -55,3 +56,22 @@ class Commands:
             return print("mission was successful")
         else:
             return print("it doesn't exist")
+    
+    def update(self):
+        title = interface.get_info(name="title to update", prefix="give the")
+        check = db.check_password_exist(title)
+        if check is False:
+            return print("this title doesn't exist")
+        else:
+            new_title = interface.get_info(name="title", prefix="inter new name as a")
+            check_title = db.check_password_exist(new_title)
+            if check_title is True:
+                return print("this title already exist")
+            else:
+                option = interface.options()
+                if option:
+                    password = gen.generate(option)
+                    db.add_password(new_title, password)
+                    return
+                else:
+                    return
